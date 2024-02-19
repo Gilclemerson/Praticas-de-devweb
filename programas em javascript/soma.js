69cf42b149cdd413e99cc793cf6d94fb2cb86f32
noresm <gilclemerson@gmail.com>
2024-02-19 17:56:04 -0300

    Praticas de estudos de javascript

---
+++ programas em javascript/soma.js
@@ -0,0 +1,10 @@
+
+alert("seja bem vindo")
+var nome = prompt(" Qual seu nome ?")
+var saudacao = alert("fiquei avontade " + nome)
+var pergunta = prompt('quanto é 10 + 5 ?')
+var resultado = pergunta 
+if( resultado == 15 )
+ console.log("vc acertou")
+else
+console.log("vc errou estude mais !!!" )
